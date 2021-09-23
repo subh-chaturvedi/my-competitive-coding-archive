512505f8a92dca3161b4ea3723782a6720a587b0
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2021-09-23 20:31:07 +0530

    Create python_solution.py

---
+++ codechef/contests/September Cook-Off 2021 Division 3/Fake GCD/python_solution.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for i in range(int(input())):
+    x=int(input())
+    
+    listfinal=""
+    
+    for i in range(1,x+1):
+        listfinal=listfinal+str(i)+" "
+
+    listfinal=listfinal[:-1]
+    print(listfinal)
+            
+    
