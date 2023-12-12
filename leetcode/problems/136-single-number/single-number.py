9a471cfd693c6737058ae4d185b79340f1725d6a
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-12 21:10:38 +0530

    Time: 124 ms (60.55%) | Memory: 18.9 MB (83.56%) - LeetSync

--- leetcode/problems/136-single-number/single-number.py
+++ leetcode/problems/136-single-number/single-number.py
@@ -1,11 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(0,len(nums),2):
-            if i==len(nums)-1:
-                return nums[i]
-            elif nums[i] == nums[i+1]:
-                continue
-            
-            else:
-                return nums[i]
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        return xor
