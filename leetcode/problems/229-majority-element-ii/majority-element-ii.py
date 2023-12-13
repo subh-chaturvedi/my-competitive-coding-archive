dc4de61cbf63270a314aa3c1efe7fee7a29465b0
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-13 23:14:13 +0530

    Time: 111 ms (55.37%) | Memory: 17.7 MB (80.08%) - LeetSync

--- leetcode/problems/229-majority-element-ii/majority-element-ii.py
+++ leetcode/problems/229-majority-element-ii/majority-element-ii.py
@@ -1,27 +1,15 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
+    def majorityElement(self, nums):
         nums.sort()
-
-        maxc= int(len(nums)/3)
-        c=1
-        ans=[]
-
-        if len(nums)==1:
-            return nums
-        if len(nums)==2:
-            if nums[0]==nums[1]:
-                return [nums[1]]
+        result = []
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
             else:
-                return nums
-
-        for i in range(1,len(nums)):
-            print(maxc,i,nums[i],c)
-            if nums[i]==nums[i-1]:
-                c+=1
-                if c>maxc:
-                    ans.append(nums[i])
-                    c=-len(nums)
-            else:
-                c=1
-
-        return ans
+                if count > len(nums) // 3:
+                    result.append(nums[i - 1])
+                count = 1
+        if count > len(nums) // 3:
+            result.append(nums[-1])
+        return result
