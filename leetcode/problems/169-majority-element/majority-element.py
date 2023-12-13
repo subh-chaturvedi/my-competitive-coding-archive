a7487440a70af7d5f0095a86fc007e84d5439b9e
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-13 22:39:18 +0530

    Time: 145 ms (81.79%) | Memory: 17.9 MB (49.25%) - LeetSync

--- leetcode/problems/169-majority-element/majority-element.py
+++ leetcode/problems/169-majority-element/majority-element.py
@@ -1,16 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a=0
-        b=0
+        nums.sort()
 
-        while True:
-            if b==b==len(nums)-1 or len(nums)==1:
-                break
-            if nums[b]==nums[a]:
-                b+=1
-                continue
-            nums.pop(a)
-            nums.pop(b-1)
-            b=b-1
+        x = nums[int(len(nums)/2)]
 
-        return nums[0]
+        return x
