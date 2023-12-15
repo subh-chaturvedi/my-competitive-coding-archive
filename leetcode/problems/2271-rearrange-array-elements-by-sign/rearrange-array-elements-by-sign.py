2234cfda757345055ca821496e039c8ed861dc3a
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-15 10:54:07 +0530

    Time: 1210 ms (42.59%) | Memory: 47.5 MB (25.36%) - LeetSync

--- leetcode/problems/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ leetcode/problems/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,29 +1,8 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        i=0
-        ic=0
-        j=0
-        jc=0
-        x=[]
-
-        while len(x)!=len(nums):
-            #print(i,j,ic,jc)
-            if i < len(nums):
-                if nums[i] <0:
-                    i+=1
-            if i < len(nums):
-                if nums[i] >0 and ic==jc:
-                    x.append(nums[i])
-                    i+=1
-                    ic+=1
-            if j < len(nums):
-                if nums[j]>0:
-                    j+=1
-            if j < len(nums):    
-                if nums[j] <0 and ic==jc+1:
-                    x.append(nums[j])
-                    j+=1
-                    jc+=1
-
-        return x
-                
+        pos = [p for p in nums if p > 0]
+        neg = [n for n in nums if n < 0]
+        ans = []
+        for p, n in zip(pos, neg):
+            ans += [p, n]
+        return ans
