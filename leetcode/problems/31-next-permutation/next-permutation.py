fbf5a7fa704a32c159747289a7c49893c604761d
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-15 22:21:28 +0530

    Time: 29 ms (99.97%) | Memory: 16.3 MB (24.79%) - LeetSync

---
+++ leetcode/problems/31-next-permutation/next-permutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def justBig(self,x,llist):
+        y=[]
+        for i in llist:
+            if i>x:
+                y.append(i)
+        print(y)
+        return min(y)
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        
+
+        for i in range(len(nums)-1,0,-1):
+            print(i)
+            if nums[i]>nums[i-1]:
+                x= self.justBig(nums[i-1],nums[i:])
+                
+                notnums=nums[i-1:]
+                notnums.remove(x)
+                notnums.sort()
+                nums[:]=nums[:i-1]+[x]+notnums
+                break
+                
+            if i==1:
+                nums[:] = nums[::-1]
+                break
+        
