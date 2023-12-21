c20d79c5d7ae1e035c9cf1043868f1b12c7901e2
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-21 12:28:00 +0530

    Time: 53 ms (67.93%) | Memory: 18 MB (9.34%) - LeetSync

---
+++ leetcode/problems/35-search-insert-position/search-insert-position.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left=0
+        right= len(nums)-1
+
+        while left<=right:
+
+            mid=(left+right)//2
+
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                right = mid-1
+            else:
+                left = mid+1
+        
+        if nums[mid]>target:
+            return mid
+        else:
+            return mid+1
