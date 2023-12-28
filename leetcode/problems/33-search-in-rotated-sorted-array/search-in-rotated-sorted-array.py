0939775a4e871c265f5d0e05413e10dc2d26eb8d
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2023-12-28 22:57:03 +0530

    Time: 85 ms (6.94%) | Memory: 17.9 MB (7.89%) - LeetSync

---
+++ leetcode/problems/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def binsearch(self,arr,k):
+        print(arr,k)
+        s,e=0,len(arr)-1
+
+        while s<=e:
+            mid= (s+e)//2
+
+            if arr[mid]==k:
+                return mid
+            elif arr[mid]<k:
+                s=mid+1
+            else:
+                e=mid-1
+        
+        return -1
+
+    def search(self, nums: List[int], target: int) -> int:
+
+        if len(nums)==1:
+            if nums[0]==target:
+                return 0
+            else:
+                return -1
+        
+        for i in range(1,len(nums)):
+            if nums[i]<nums[i-1]:
+                if target>=nums[0]:
+                    return self.binsearch(nums[:i],target)
+                else:
+                    pos = self.binsearch(nums[i:],target)
+                    if pos==-1:
+                        return pos
+                    return i+pos
+        
+        return self.binsearch(nums,target)
