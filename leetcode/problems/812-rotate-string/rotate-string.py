068589f1d7acbbd307e6de118fc2953360f8e76e
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-01-04 19:43:20 +0530

    Time: 38 ms (55.99%) | Memory: 17.2 MB (13.63%) - LeetSync

---
+++ leetcode/problems/812-rotate-string/rotate-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        
+        for i in range(len(s)):
+
+            if s[i:]+s[:i] == goal:
+                return True
+        
+        return False
