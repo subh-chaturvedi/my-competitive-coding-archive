acca6360ef341671479da441f9e4876119040119
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-03-01 11:54:22 +0530

    Time: 41 ms (20.19%) | Memory: 16.5 MB (49.63%) - LeetSync

--- leetcode/problems/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ leetcode/problems/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -8,7 +8,7 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        # if root.right:
+        
         return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
 
-        # return self.inorderTraversal(root.left)+[root.val]
+        
