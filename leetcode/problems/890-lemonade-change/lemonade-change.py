7ac3e51d122b16be2e19a2cb165703412b5f2cb8
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-03-09 22:40:48 +0530

    Time: 619 ms (72.86%) | Memory: 21.2 MB (59.05%) - LeetSync

--- leetcode/problems/890-lemonade-change/lemonade-change.py
+++ leetcode/problems/890-lemonade-change/lemonade-change.py
@@ -3,7 +3,7 @@ class Solution:
         
         c1 = 0
         c2 = 0
-        c3 = 0
+        
 
         for i in bills:
             if i==5:
@@ -12,13 +12,13 @@ class Solution:
                 c2+=1
                 c1-=1
             else:
-                c3+=1
+                
                 if c2>0:
                     c2-=1
                     c1-=1
                 else:
                     c1-=3
-            if c1<0 or c2<0 or c3<0:
+            if c1<0 or c2<0:
                 return False
         
         return True
