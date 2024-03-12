a29b22df5de4f8c4fdfbf30801f72898bdbefb27
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-03-12 14:38:41 +0530

    Time: 97 ms (93.46%) | Memory: 17.8 MB (27.63%) - LeetSync

--- leetcode/problems/45-jump-game-ii/jump-game-ii.py
+++ leetcode/problems/45-jump-game-ii/jump-game-ii.py
@@ -3,23 +3,25 @@ class Solution:
 
         ans=0
 
-        if len(nums)<2:
+        if len(nums)<2: #Edge case where the array is of length 1 or 0
             return ans
 
-        start=0
-        end=0
+        end=0 
         maxend=0
         i=0
 
-        while True:
+        while True: #Constraint is that every test case is valid for the jump game so this loop has to eventually end by return
             while i<=end:
                 if nums[i]+i > maxend:
                     maxend=nums[i]+i
                 if maxend >= len(nums)-1:
                     return ans+1
                 i+=1
+                # if i>=len(nums):
+                #     break
             if end < maxend:
                 end=maxend
                 ans+=1
-        
+            # else:
+            #     break
         return ans
