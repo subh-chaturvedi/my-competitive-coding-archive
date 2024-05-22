34dd2e439b6e935433721c7f3c24d653641d24e1
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-05-22 14:04:43 +0530

    Time: 453 ms (85.55%) | Memory: 44.7 MB (5.02%) - LeetSync

---
+++ leetcode/problems/131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    @cache  # the memory trick can save some time
+    def partition(self, s):
+        if not s: return [[]]
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:  # prefix is a palindrome
+                for suf in self.partition(s[i:]):  # process suffix recursively
+                    ans.append([s[:i]] + suf)
+        return ans
