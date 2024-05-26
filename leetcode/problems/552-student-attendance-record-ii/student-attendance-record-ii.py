c71f3ac09959fba5e0ab598e433837270cc41b13
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-05-26 15:51:55 +0530

    Time: 120 ms (95.73%) | Memory: 36.5 MB (59.72%) - LeetSync

---
+++ leetcode/problems/552-student-attendance-record-ii/student-attendance-record-ii.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class Solution:
+    
+    def checkRecord(self, n: int) -> int:
+        MODULUS = 10**9 + 7
+
+        initial_counts = np.array(
+            [1, 0, 0, 0, 0, 0], 
+            dtype=np.int64
+        )
+
+        adjacency_matrix = np.array([
+            [1, 1, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+        ], dtype=np.int64)
+
+        def power(A, exp):
+            B = np.identity(len(A), dtype=np.int64)
+            for bit in reversed(bin(exp)[2:]):
+                if bit == '1':
+                    B = B @ A
+                    B %= MODULUS
+                A = A @ A
+                A %= MODULUS
+            return B
+
+        final_counts = power(adjacency_matrix, n) @ initial_counts
+
+        return sum(final_counts) % MODULUS
