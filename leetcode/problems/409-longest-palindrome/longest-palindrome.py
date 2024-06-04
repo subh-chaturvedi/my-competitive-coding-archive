05dc89140cd3f27715d47d7c7e248e7f5891c1cc
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-06-04 14:11:18 +0530

    Time: 43 ms (15.94%) | Memory: 16.6 MB (34.50%) - LeetSync

---
+++ leetcode/problems/409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self,s):
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
+            
+            
