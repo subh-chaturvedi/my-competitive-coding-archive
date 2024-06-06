a084983156e160c883bc8787e8a88ee137a17e1a
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-06-06 15:00:05 +0530

    Time: 498 ms (12.39%) | Memory: 18.4 MB (50.76%) - LeetSync

---
+++ leetcode/problems/876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isNStraightHand(self, nums: List[int], k: int) -> bool:
+        if len(nums) % k != 0:
+            return False
+
+        numsDict = {}
+
+
+        for i in nums:
+            if i in numsDict:
+                numsDict[i]+=1
+            else:
+                numsDict[i]=1
+        numsLeft = len(nums)
+
+        while numsLeft>0:
+            minI= min(numsDict.keys())
+            for i in range(k):
+                if minI+i not in numsDict:
+                    return False
+                else:
+                    numsDict[minI+i]-=1
+                    numsLeft-=1
+                if numsDict[minI+i] == 0:
+                    del numsDict[minI+i]
+        return True
