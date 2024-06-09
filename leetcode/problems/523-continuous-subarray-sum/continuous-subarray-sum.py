6c65849504a6777cfa7350ea5107899f6e46613f
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-06-09 15:53:10 +0530

    Time: 913 ms (5.41%) | Memory: 36.7 MB (10.53%) - LeetSync

---
+++ leetcode/problems/523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+        cache = {0:-1}
+        remsum=0
+
+        for i,num in enumerate(nums):
+            print(i)
+            remsum = (remsum+num)%k
+
+            if remsum in cache:
+                if i-cache[remsum]>1:
+                    return True
+            else:
+                cache[remsum]=i
+
+        return False        
+
+# 5 1 5
+# 23 2 4 6
+# 0 0 2 2
+        
+        
