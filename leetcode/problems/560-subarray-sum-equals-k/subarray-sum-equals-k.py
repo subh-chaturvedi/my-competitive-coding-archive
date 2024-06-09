25467fcdb456c00984de0922b08cae700cc4356e
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-06-09 14:24:53 +0530

    Time: 220 ms (84.35%) | Memory: 19.4 MB (91.05%) - LeetSync

--- leetcode/problems/560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ leetcode/problems/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,22 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        prefSumsD = {0:1}
+        freq, prefix_sum, total = {0:1}, 0, 0
 
-        prefsum = 0
+        for num in nums:
+            prefix_sum += num
+            total += freq.get(prefix_sum-k, 0)
+            freq[prefix_sum] = freq.get(prefix_sum, 0) + 1
 
-        ans=0
-
-        for i in nums:
-            prefsum+=i
-
-            if (prefsum-k) in prefSumsD:
-                ans+=prefSumsD[prefsum-k]
-            
-            if prefsum in prefSumsD:
-                prefSumsD[prefsum]+=1
-            else:
-                prefSumsD[prefsum]=1
-            
-        
-        return ans
+        return total
