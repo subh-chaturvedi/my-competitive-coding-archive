b159505d1866951fa8571401c7b16d91fc853c73
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-06-12 22:00:45 +0530

    Time: 35 ms (73.49%) | Memory: 16.5 MB (79.02%) - LeetSync

--- leetcode/problems/75-sort-colors/sort-colors.py
+++ leetcode/problems/75-sort-colors/sort-colors.py
@@ -1,4 +1,18 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
-        
+       
+        zeros, ones, n = 0, 0, len(nums)
+        for num in nums:
+            if num == 0:
+                zeros += 1
+            elif num == 1:
+                ones += 1
+
+        for i in range(0, zeros):
+            nums[i] = 0
+
+        for i in range(zeros, zeros + ones):
+            nums[i] = 1
+
+        for i in range(zeros + ones, n):
+            nums[i] = 2
