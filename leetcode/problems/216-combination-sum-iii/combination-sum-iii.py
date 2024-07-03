5935457b7572a5537f6980037913986db294a304
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-03 21:48:09 +0530

    Time: 631 ms (5.11%) | Memory: 16.5 MB (50.71%) - LeetSync

--- leetcode/problems/216-combination-sum-iii/combination-sum-iii.py
+++ leetcode/problems/216-combination-sum-iii/combination-sum-iii.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ans=[]
-        def f(curr):
+        def f(curr,s):
             if len(curr)==k:
-                if sum(curr)==n:
+                if s==n:
                     curr.sort()
                     if curr not in ans:
                         ans.append(curr)
                 return
-            if sum(curr)>=n:
+            if s>=n:
                 return
             
             for i in range(1,10):
-                if sum(curr)+i<=n and i not in curr:
-                    f(curr+[i])
+                if s+i<=n and i not in curr:
+                    f(curr+[i],s+i)
                 
-        f([])
+        f([],0)
 
         return ans
