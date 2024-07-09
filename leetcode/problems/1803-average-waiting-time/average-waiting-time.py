1810954bf5e8c1ff5c5abc46c6fa539f03fa6863
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-09 11:41:09 +0530

    Time: 772 ms (22.34%) | Memory: 57.2 MB (9.97%) - LeetSync

---
+++ leetcode/problems/1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,18 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        
+        t=1
+
+        ans  = 0
+
+        for cust in customers:
+            if t<cust[0]:
+                t=cust[0]
+            
+            t+=cust[1]
+
+            ans+= (t-cust[0])
+        
+        ans = ans/len(customers)
+
+        return ans
