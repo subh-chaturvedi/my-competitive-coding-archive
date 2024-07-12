fd049d4f50073c37b0ae327e9919c737334c0807
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-12 15:25:48 +0530

    Time: 34 ms (57.39%) | Memory: 16.6 MB (25.42%) - LeetSync

--- leetcode/problems/292-nim-game/nim-game.py
+++ leetcode/problems/292-nim-game/nim-game.py
@@ -1,14 +1,3 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
-        
-        # rem = n%6
-
-        # if rem<=3 and rem>0:
-        #     return True
-        # else:
-        #     return False
-
-        if n%4==0:
-            return False
-        else:
-            return True
+        return n % 4 != 0
