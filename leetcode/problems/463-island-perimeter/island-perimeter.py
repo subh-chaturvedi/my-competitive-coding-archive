83b43ba26e05dac6df408419a56032e456b22173
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-12 17:01:08 +0530

    Time: 378 ms (60.47%) | Memory: 16.9 MB (54.15%) - LeetSync

--- leetcode/problems/463-island-perimeter/island-perimeter.py
+++ leetcode/problems/463-island-perimeter/island-perimeter.py
@@ -6,18 +6,18 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    print(i,j)
+                    # print(i,j)
                     if i+1>=len(grid) or grid[i+1][j] == 0:
                         ans+=1
-                        print("top")
+                        # print("top")
                     if i-1<0 or grid[i-1][j] == 0:
                         ans+=1
-                        print("bottom")
+                        # print("bottom")
                     if j+1>=len(grid[0]) or grid[i][j+1] == 0:
                         ans+=1
-                        print("right")
+                        # print("right")
                     if j-1<0 or grid[i][j-1] == 0:
                         ans+=1
-                        print("left")
+                        # print("left")
         
         return ans
