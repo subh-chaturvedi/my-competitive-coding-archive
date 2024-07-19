f94b619116d55a1f750404222a2b20a75ef5ea62
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-19 15:48:56 +0530

    Time: 312 ms (8.89%) | Memory: 55.7 MB (21.28%) - LeetSync

---
+++ leetcode/problems/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,20 @@
+def max_profit(prices, i, n, can_sell, mem):
+    if i >= len(prices) or n == 0: 
+        # We have reached the end of the prices array, or can't perform any more transactions
+        # Our profit from this point on is 0 in this case
+        return 0
+    elif (i, n, can_sell) in mem:
+        # This state was previously encountered - return the memorized value
+        return mem[(i, n, can_sell)]
+    
+    # Buy the stock if we aren't holding any, or sell it otherwise
+    do = max_profit(prices, i+1, n-1, not can_sell, mem) + (prices[i] if can_sell else -prices[i])
+    # Skip the position without buying/selling
+    skip = max_profit(prices, i+1, n, can_sell, mem)
+    
+    mem[(i, n, can_sell)] = max(do, skip)
+    return mem[(i, n, can_sell)]
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        return max_profit(prices, 0, 2*k, False, {})
