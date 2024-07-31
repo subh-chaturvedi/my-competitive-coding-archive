2d4bf02338e528d2c90cec59e48ca9f4c915f9e9
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-31 15:06:32 +0530

    Time: 89 ms (91.49%) | Memory: 17.5 MB (37.93%) - LeetSync

--- leetcode/problems/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ leetcode/problems/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,18 +1,20 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         
-        stck = []
+        stck = ['0']
         ans=0
 
         for i in s:
             # print(stck,i)
-            if not stck:
-                stck.append(i)
-                continue
+            # if not stck:
+            #     stck.append(i)
+            #     continue
 
             if stck[-1]=="0":
+                
                 stck.append(i)
             else:
+                
                 if i=="1":
                     stck.append(i)
                 else:
