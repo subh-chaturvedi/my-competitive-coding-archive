9a039a3a5c62cddf062e4e1aee6215a3e24958e3
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-08-10 12:14:54 +0530

    Time: 124 ms (39.80%) | Memory: 17.8 MB (74.06%) - LeetSync

--- leetcode/problems/130-surrounded-regions/surrounded-regions.py
+++ leetcode/problems/130-surrounded-regions/surrounded-regions.py
@@ -3,48 +3,24 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-
-        queue=[]
-
-
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         def dfs(r,c):
-            # print("DFS called",r,c)
-
             if r<0 or c<0 or r>= len(board) or c>= len(board[0]):
                 return
-
             if board[r][c] == "X" or board[r][c]=="S":
                 return
-
             board[r][c] = "S"
-
             for rr, cc in directions:
-                # print("calling further",rr,cc)
                 dfs(r+rr,c+cc)
-
             return
-        
         
         for r in range(len(board)):
             for c in range(len(board[0])):
                 if r==0 or r==len(board)-1 or c==0 or c==len(board[0])-1:
                     if board[r][c]=="O":
                         dfs(r,c)
-                        # board[r][c]="S"
-                        # queue.append((r,c))
 
-        
-        # print(board)
-        # print(queue)
-
-        
-
-        # for r,c in queue:
-        #     dfs(r,c,True)
-        
-        # print(board)
         for r in range(len(board)):
             for c in range(len(board[0])):
 
